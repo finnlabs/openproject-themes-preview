b120e58f92c7267e3d1cff8df4b4164e3541e039
Christian Ratz <c.ratz@finn.de>
2014-01-03 08:55:02 +0100

    fixed it

--- lib/open_project/themes/preview.rb
+++ lib/open_project/themes/preview.rb
@@ -1,9 +1,11 @@
-module OpenProject::Themes
-  module Preview
-    require 'open_project/themes/preview/engine'
+module OpenProject
+  module Themes
+    module Preview
+      require 'open_project/themes/preview/engine'
 
-    def self.assets_path
-      @assets_path ||= Engine.root.join('app/assets').to_s
+      def self.assets_path
+        @assets_path ||= Engine.root.join('app/assets').to_s
+      end
     end
   end
 end
